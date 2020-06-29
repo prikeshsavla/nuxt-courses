f194049e42eb5f85ee2099fb0ce07883daa2087e
Prikesh Savla <prikesh@rirev.com>
2020-06-29 23:18:51 +0530

    Add Initial template

--- pages/index.vue
+++ pages/index.vue
@@ -3,7 +3,7 @@
     <div>
       <logo />
       <h1 class="title">
-        nuxt-promo-template
+        nuxt-promo-yourself
       </h1>
       <h2 class="subtitle">
         My super Nuxt.js project
